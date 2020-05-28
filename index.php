9ffa2f494308ce68bd11a4384c4652862fd5f8b0
2102760 <35136841+2102760@users.noreply.github.com>
2020-05-28 11:33:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ require_once("MyLogger.class.php");
 $logger = new MyLogger('TestClass');
 $logger->log("Hello World", 'warning');
 
-$logger2= new MyLogger('TestClass2');
+$logger2 = new MyLogger('TestClass2');
 $logger2->log("Hello World", 'warning');
 ?>
