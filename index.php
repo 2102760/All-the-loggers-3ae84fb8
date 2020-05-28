7050a47ddd7eac2b7cd6b9b637abacaeea8cb363
BeauvR <beauvrmc140@gmail.com>
2020-05-28 11:19:52 +0200

    uitwerking

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
 require_once("MyLogger.class.php");
-$logger = new MyLogger();
-$logger->setOrigin('TestClass');
+$logger = new MyLogger('TestClass');
 $logger->log("Hello World", 'warning');
+
+$logger2= new MyLogger('TestClass2');
+$logger2->log("Hello World", 'warning');
 ?>
